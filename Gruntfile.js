e742983c080a4f423df7239b1f30146b90e5813d
Blaine Jester <blainesworld@gmail.com>
2014-03-15 12:30:33 -0700

    Updated Grunt file with structure updates and removed circular reference

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,10 +17,24 @@ module.exports = function(grunt)
       {
         src: 
         [
-          'lib/particle.js',
-          'lib/electron.js',
-          'lib/proton.js',
-          'lib/neutron.js'
+          // Base
+          'lib/Util.js',
+          'lib/exception/Exception.js',
+          'lib/particle/Particle.js',
+          'lib/analyzer/Analyzer.js',
+          
+          // Dependee's
+          'lib/util/Logger.js',
+          'lib/exception/InvalidPreparation.js',
+          'lib/particle/Electron.js',
+          'lib/particle/Neutron.js',
+          'lib/particle/Proton.js',
+          'lib/analyzer/SternGerlach.js',
+          'lib/analyzer/MagneticField.js',
+          
+          // Core (final)
+          'lib/State.js',
+          'lib/System.js'
         ],
         dest: 'build/<%= pkg.name %>.min.js'
       }
