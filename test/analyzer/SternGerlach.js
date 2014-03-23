58061a6a586d372612a62275eaae9e14f63b39a8
Blaine Jester <blainesworld@gmail.com>
2014-03-23 11:01:29 -0700

    Added new test for 50-50 case, and fixed issues with probability counting

--- test/analyzer/SternGerlach.js
+++ test/analyzer/SternGerlach.js
@@ -40,14 +40,30 @@ describe('Analyzer.SternGerlach', function()
   
   describe('analyze', function()
   {
-    it('should produce the normal 50-50 results', function()
+    System().setParticle(Particle.Electron);
+    
+    it('should produce an absolute result', function()
     {
       // State in Z
       var state = getState();
-      System().setParticle(Particle.Electron);
          
       var results = getAnalyzer().analyze(state);
       assert.equal(1000, results[0].getParticles().length);
+    });
+    
+    it('should produce a 50-50 probability result', function()
+    {
+      // State in up Z
+      var state = getState();
+      state.getVector().setComponent(1,0).normalize();
+      
+      // Its possible for this to fail, since its base off of probabilities
+      // but most of the time it should be around a 500 500 split
+      var results = getAnalyzer().analyze(state);
+      assert.equal(true, 
+        test.withinRange(500, results[0].getParticles().length, 50));
+      assert.equal(1000,
+        results[0].getParticles().length + results[1].getParticles().length);
     });
   });
   
