5aea2d6c5c86507a39af254d7df0973dcdd8eb4d
Blaine Jester <blainesworld@gmail.com>
2014-03-27 20:30:32 -0700

    Fixed issue with probability algorithm and updated tests to cover the issue

--- test/analyzer/SternGerlach.js
+++ test/analyzer/SternGerlach.js
@@ -24,17 +24,19 @@ describe('Analyzer.SternGerlach', function()
   /**
    * @returns {State}
    */
-  var getState = function()
+  var getState = function(orient)
   {
-    return new State(getParticle(1000), new Util.Vector(State.SPIN_HALF.Z));
+    orient = orient || State.SPIN_HALF.Z;
+    return new State(getParticle(1000), new Util.Vector(orient));
   };
   
   /**
    * @returns {Analyzer.SternGerlach}
    */
-  var getAnalyzer = function()
+  var getAnalyzer = function(orient)
   {
-    var v = new Util.Vector(State.SPIN_HALF.X);
+    orient = orient || State.SPIN_HALF.X;
+    var v = new Util.Vector(orient);
     return new Analyzer.SternGerlach(v);
   };
   
@@ -65,6 +67,51 @@ describe('Analyzer.SternGerlach', function()
       assert.equal(1000,
         results[0].getParticles().length + results[1].getParticles().length);
     });
+
+    it('should verify results from a first analyzer', function()
+    {
+      // State in up Z
+      var state = getState();
+      state.getVector().setComponent(1,0).normalize();
+
+      // Generate a 50-50
+      var firstResult = getAnalyzer().analyze(state);
+
+      var upResult = getAnalyzer().analyze(firstResult[0]);
+      var downResult = getAnalyzer().analyze(firstResult[1]);
+
+      assert.equal(1000, upResult[0].getParticles().length
+        + downResult[1].getParticles().length);
+    });
+
+    // Spin 1 type test, also makes sure that probabilities are in order
+    it('should result in the correct probabilities', function()
+    {
+      // State in up Z
+      var basis = [ [[1],[0],[0]], [[0],[1],[0]], [[0],[0],[1]]]
+        .map(function(val)
+        {
+          return math.matrix(val);
+        });
+      var comps = [1, math.sqrt(3), math.sqrt(6)];
+
+      var analyzer = new Analyzer.SternGerlach(new Util.Vector(basis));
+      var state = new State(getParticle(10000), new Util.Vector(basis));
+      state
+        .getVector()
+        .setComponents(comps)
+        .normalize();
+
+      var results = analyzer.analyze(state), counts = [];
+
+      for (var i = 0; i < results.length; i++)
+      {
+        var num = comps[i] * comps[i] * 1000,
+            count = results[i].getParticles().length;
+
+        assert.equal(true, test.withinRange(num, count, 100));
+      }
+    });
   });
-  
+
 });
